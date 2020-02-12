510c2f36a9f5db0a202d8bd0a400900707165c93
kostispar <30313084+kostispar@users.noreply.github.com>
2020-02-12 21:18:40 +0200

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -11,103 +11,6 @@ import matplotlib.pyplot as plt
 import sys
 import re
 
-def segs2flags(seg_start, seg_end, seg_label, win_size):
-    '''
-    This function converts segment endpoints and respective segment
-    labels to fix-sized class labels.
-    ARGUMENTS:
-     - seg_start:    segment start points (in seconds)
-     - seg_end:    segment endpoints (in seconds)
-     - seg_label:    segment labels
-      - win_size:    fix-sized window (in seconds)
-    RETURNS:
-     - flags:    numpy array of class indices
-     - class_names:    list of classnames (strings)
-    '''
-    flags = []
-    class_names = list(set(seg_label))
-    curPos = win_size / 2.0
-    while curPos < seg_end[-1]:
-        for i in range(len(seg_start)):
-            if curPos > seg_start[i] and curPos <= seg_end[i]:
-                break
-        flags.append(class_names.index(seg_label[i]))
-        curPos += win_size
-    return numpy.array(flags), class_names
-
-
-
-def readSegmentGT(gt_file):
-    '''
-    This function reads a segmentation ground truth file, following a simple CSV format with the following columns:
-    <segment start>,<segment end>,<class label>
-
-    ARGUMENTS:
-     - gt_file:       the path of the CSV segment file
-    RETURNS:
-     - seg_start:     a numpy array of segments' start positions
-     - seg_end:       a numpy array of segments' ending positions
-     - seg_label:     a list of respective class labels (strings)
-    '''
-    f = open(gt_file, 'rt')
-    reader = csv.reader(f, delimiter=',')
-    seg_start = []
-    seg_end = []
-    seg_label = []
-    for row in reader:
-        if len(row) == 3:
-            seg_start.append(float(row[0]))
-            seg_end.append(float(row[1]))
-            #if row[2]!="other":
-            #    seg_label.append((row[2]))
-            #else:
-            #    seg_label.append("silence")
-            seg_label.append((row[2]))
-    return numpy.array(seg_start), numpy.array(seg_end), seg_label
-
-
-
-def flags2segs(flags, window):
-    '''
-    ARGUMENTS:
-     - flags:      a sequence of class flags (per time window)
-     - window:     window duration (in seconds)
-
-    RETURNS:
-     - segs:       a sequence of segment's limits: segs[i,0] is start and
-                   segs[i,1] are start and end point of segment i
-     - classes:    a sequence of class flags: class[i] is the class ID of
-                   the i-th segment
-    '''
-
-    preFlag = 0
-    cur_flag = 0
-    n_segs = 0
-
-    cur_val = flags[cur_flag]
-    segsList = []
-    classes = []
-    while (cur_flag < len(flags) - 1):
-        stop = 0
-        preFlag = cur_flag
-        preVal = cur_val
-        while (stop == 0):
-            cur_flag = cur_flag + 1
-            tempVal = flags[cur_flag]
-            if ((tempVal != cur_val) | (cur_flag == len(flags) - 1)):  # stop
-                n_segs = n_segs + 1
-                stop = 1
-                cur_seg = cur_val
-                cur_val = flags[cur_flag]
-                segsList.append((cur_flag * window))
-                classes.append(preVal)
-    segs = numpy.zeros((len(segsList), 2))
-
-    for i in range(len(segsList)):
-        if i > 0:
-            segs[i, 0] = segsList[i-1]
-        segs[i, 1] = segsList[i]
-    return (segs, classes)
 
 def FileClassification(input_file, model_name, model_type, gt = False, gt_file=""):
     '''
@@ -203,13 +106,13 @@ def FileClassification(input_file, model_name, model_type, gt = False, gt_file="
         if flags_ind[i - 1] == flags_ind[i + 1]:
             flags_ind[i] = flags_ind[i + 1]
     # convert fix-sized flags to segments and classes
-    (segs, classes) = flags2segs(flags, mt_step)
+    (segs, classes) = aS.flags2segs(flags, mt_step)
     segs[-1] = len(x) / float(fs)
     if gt == True:
         # Load grount-truth:
         if os.path.isfile(gt_file):
-            [seg_start_gt, seg_end_gt, seg_l_gt] = readSegmentGT(gt_file)
-            flags_gt, class_names_gt = segs2flags(seg_start_gt, seg_end_gt, seg_l_gt, mt_step)
+            [seg_start_gt, seg_end_gt, seg_l_gt] = aS.readSegmentGT(gt_file)
+            flags_gt, class_names_gt = aS.segs2flags(seg_start_gt, seg_end_gt, seg_l_gt, mt_step)
             flags_ind_gt = []
             # print(class_names)
             for j, fl in enumerate(flags_gt):
@@ -282,7 +185,7 @@ def plotSegmentationResults(flags_ind, flags_ind_gt, class_names, mt_step, ONLY_
     It also computes the overall accuracy achieved by the respective method if ground-truth is available.
     '''
     flags = [class_names[int(f)] for f in flags_ind]
-    (segs, classes) = flags2segs(flags, mt_step)
+    (segs, classes) = aS.flags2segs(flags, mt_step)
     min_len = min(flags_ind.shape[0], flags_ind_gt.shape[0])
     if min_len > 0:
         accuracy = numpy.sum(flags_ind[0:min_len] ==
